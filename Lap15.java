216851ed29cd32a43f35b6d65454ad91e045eac1
Abdulkhaleq720 <112189924+Abdulkhaleq720@users.noreply.github.com>
2022-10-11 15:08:09 +0300

    Delete Lap15.java

--- Lap15.java
+++
@@ -1,7 +0,0 @@
-public class Lap15 {
-    public static void main(String[] args) {
-        System.out.println("----------------");
-        System.out.println("| Ibraheem      |");
-        System.out.println("----------------");
-    }
-}
