26241da30f99a3de0dba54c6521f6f0f514990cf
Abdulkhaleq720 <112189924+Abdulkhaleq720@users.noreply.github.com>
2022-11-10 23:31:41 +0300

    Add files via upload

---
+++ مشروع/src/Family.java
@@ -0,0 +1,12 @@
+public class Family extends Persun {
+    public Family (String fname,String sname,int a)
+    {
+        super(fname,sname,a);
+    }
+    public void displayDetails()
+    {
+        super.displayDetails();
+    }
+}
+
+
